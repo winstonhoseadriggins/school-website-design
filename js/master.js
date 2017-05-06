e70ac31c07ea9a2a809446b2392068e9dd4e03b8
Mohammad Mohibbullah <mbmohib@gmail.com>
2017-05-06 11:31:17 +0600

    Desing nav shrink

--- js/master.js
+++ js/master.js
@@ -1,4 +1,14 @@
+// Wow JS
 new WOW().init();
+
+// For reducing nav height
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink segment');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
 
 // Dropdown Initialize
 $('.ui.dropdown').dropdown({
