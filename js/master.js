bddfb62264bd12b336e07638dbdeb0e3196d031e
Mohammad Mohibbulllah <mbmohib@gmail.com>
2017-05-15 14:47:04 +0600

    Add carousel

--- js/master.js
+++ js/master.js
@@ -14,3 +14,18 @@ $(window).scroll(function() {
 $('.ui.dropdown').dropdown({
     on : 'hover'
 });
+
+
+$('.header-carousel').owlCarousel({
+	autoplay:true,
+	autoplayTimeout:3000,
+	animateOut: 'slideOutDown',
+	animateIn: 'flipInX',
+	loop:true,
+	items: 1,
+	margin: 30,
+	stagePadding: 30,
+	smartSpeed: 450,
+	lazyLoad : true,
+	margin:100
+});
